2412509e52e763672a03e4ffdfe2ef71afc181b8
Evgeny Vintik <vintik.jeny@gmail.com>
2019-04-11 19:52:42 +0200

    CXF jaxws server dependencies

--- bootique-cxf-jaxws/src/main/java/io/bootique/cxf/jaxws/CxfJaxwsServerModuleProvider.java
+++ bootique-cxf-jaxws/src/main/java/io/bootique/cxf/jaxws/CxfJaxwsServerModuleProvider.java
@@ -2,10 +2,25 @@ package io.bootique.cxf.jaxws;
 
 import com.google.inject.Module;
 import io.bootique.BQModuleProvider;
+import io.bootique.cxf.CxfModuleProvider;
+import io.bootique.jetty.JettyModuleProvider;
+
+import java.util.Collection;
+
+import static java.util.Arrays.asList;
 
 public class CxfJaxwsServerModuleProvider implements BQModuleProvider {
     @Override
     public Module module() {
         return new CxfJaxwsServerModule();
+    }
+
+
+    @Override
+    public Collection<BQModuleProvider> dependencies() {
+        return asList(
+                new JettyModuleProvider(),
+                new CxfModuleProvider()
+        );
     }
 }
