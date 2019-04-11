562d41174dfdd3a0b1abfe967d93d1a625a3aa61
Evgeny Vintik <vintik.jeny@gmail.com>
2019-04-11 19:54:12 +0200

    module tests

---
+++ bootique-cxf-core/src/test/java/io/bootique/cxf/CxfModuleIT.java
@@ -0,0 +1,42 @@
+package io.bootique.cxf;
+
+import com.google.inject.Key;
+import io.bootique.BQRuntime;
+import io.bootique.test.junit.BQTestFactory;
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.extension.ExtensionManagerBus;
+import org.apache.cxf.configuration.ConfiguredBeanLocator;
+import org.apache.cxf.configuration.Configurer;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class CxfModuleIT {
+
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory().autoLoadModules();
+
+    static class TestBus extends ExtensionManagerBus {}
+
+    @Test
+    public void testOverrideBusImplementation() {
+
+        BQRuntime runtime = testFactory.app().override(CxfModule.class).with(binder -> {
+            binder.bind(Bus.class).to(TestBus.class);
+        }).createRuntime();
+
+        Assert.assertEquals(TestBus.class, runtime.getInstance(Key.get(Bus.class)).getClass());
+        Assert.assertNotNull(runtime.getInstance(Key.get(Configurer.class)));
+        Assert.assertNotNull(runtime.getInstance(Key.get(ConfiguredBeanLocator.class)));
+
+    }
+
+    @Test
+    public void testDefaultContents() {
+        BQRuntime runtime = testFactory.app().createRuntime();
+
+        Assert.assertNotNull(runtime.getInstance(Key.get(Bus.class)));
+        Assert.assertNotNull(runtime.getInstance(Key.get(Configurer.class)));
+        Assert.assertNotNull(runtime.getInstance(Key.get(ConfiguredBeanLocator.class)));
+    }
+}
---
+++ bootique-cxf-jaxws/src/test/java/io/bootique/cxf/jaxws/CxfJaxwsServerModuleProviderIT.java
@@ -0,0 +1,31 @@
+package io.bootique.cxf.jaxws;
+
+import io.bootique.BQRuntime;
+import io.bootique.cxf.CxfModule;
+import io.bootique.jetty.JettyModule;
+import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQRuntimeChecker;
+import io.bootique.test.junit.BQTestFactory;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class CxfJaxwsServerModuleProviderIT {
+
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory();
+
+    @Test
+    public void testAutoLoadable() {
+        BQModuleProviderChecker.testAutoLoadable(CxfJaxwsServerModuleProvider.class);
+    }
+
+    @Test
+    public void testModuleDeclaresDependencies() {
+        final BQRuntime bqRuntime = testFactory.app().module(new CxfJaxwsServerModuleProvider()).createRuntime();
+        BQRuntimeChecker.testModulesLoaded(bqRuntime,
+                JettyModule.class,
+                CxfModule.class,
+                CxfJaxwsServerModule.class
+        );
+    }
+}
