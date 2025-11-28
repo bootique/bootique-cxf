fe8326e80bcde9799cf7a8a89b426ca29512d55c
Andrus Adamchik <andrus@objectstyle.com>
2025-11-28 14:44:34 -0500

    Getting rid of Mockito

--- bootique-cxf-core/src/test/java/io/bootique/cxf/conf/MultisourceBeanLocatorTest.java
+++ bootique-cxf-core/src/test/java/io/bootique/cxf/conf/MultisourceBeanLocatorTest.java
@@ -1,7 +1,6 @@
 package io.bootique.cxf.conf;
 
 import org.apache.cxf.configuration.ConfiguredBeanLocator;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
@@ -10,23 +9,8 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
 
 public class MultisourceBeanLocatorTest {
-
-    private ConfiguredBeanLocator source1;
-    private ConfiguredBeanLocator source2;
-    private MultisourceBeanLocator multisourceBeanLocator;
-
-    @BeforeEach
-    public void setUp() {
-        source1 = mock(ConfiguredBeanLocator.class);
-        source2 = mock(ConfiguredBeanLocator.class);
-
-        multisourceBeanLocator = new MultisourceBeanLocator(source1, source2);
-    }
 
     @Test
     public void atLeastOneSourceIsRequired() {
@@ -36,10 +20,10 @@ public class MultisourceBeanLocatorTest {
     @Test
     public void getBeanNamesOfType_emptySource() {
 
-        when(source1.getBeanNamesOfType(eq(Long.class))).thenReturn(emptyList());
-        when(source2.getBeanNamesOfType(eq(Long.class))).thenReturn(asList("test", "test2"));
-
-        List<String> names = multisourceBeanLocator.getBeanNamesOfType(Long.class);
+        List<String> names = new MultisourceBeanLocator(
+                new BeanNamesLocator(Long.class, emptyList()),
+                new BeanNamesLocator(Long.class, asList("test", "test2"))
+        ).getBeanNamesOfType(Long.class);
 
         assertArrayEquals(asList("test", "test2").toArray(), names.toArray());
     }
@@ -47,10 +31,10 @@ public class MultisourceBeanLocatorTest {
     @Test
     public void getBeanNamesOfType_nullSource() {
 
-        when(source1.getBeanNamesOfType(eq(Long.class))).thenReturn(null);
-        when(source2.getBeanNamesOfType(eq(Long.class))).thenReturn(asList("test", "test2"));
-
-        List<String> names = multisourceBeanLocator.getBeanNamesOfType(Long.class);
+        List<String> names = new MultisourceBeanLocator(
+                new BeanNamesLocator(Long.class, null),
+                new BeanNamesLocator(Long.class, asList("test", "test2"))
+        ).getBeanNamesOfType(Long.class);
 
         assertArrayEquals(asList("test", "test2").toArray(), names.toArray());
     }
@@ -58,21 +42,20 @@ public class MultisourceBeanLocatorTest {
     @Test
     public void getBeanNamesOfType_mergeResults() {
 
-        when(source1.getBeanNamesOfType(eq(Long.class))).thenReturn(asList("_test", "_test2"));
-        when(source2.getBeanNamesOfType(eq(Long.class))).thenReturn(asList("test", "test2"));
-
-        List<String> names = multisourceBeanLocator.getBeanNamesOfType(Long.class);
+        List<String> names = new MultisourceBeanLocator(
+                new BeanNamesLocator(Long.class, asList("_test", "_test2")),
+                new BeanNamesLocator(Long.class, asList("test", "test2"))
+        ).getBeanNamesOfType(Long.class);
 
         assertArrayEquals(asList("_test", "_test2", "test", "test2").toArray(), names.toArray());
     }
 
-
     @Test
     public void getBeanOfType_nullFirstSource() {
-        when(source1.getBeanOfType(anyString(), eq(Long.class))).thenReturn(null);
-        when(source2.getBeanOfType(anyString(), eq(Long.class))).thenReturn(3L);
-
-        Long bean = multisourceBeanLocator.getBeanOfType("test", Long.class);
+        Long bean = new MultisourceBeanLocator(
+                new BeanLocator(Long.class, null),
+                new BeanLocator(Long.class, 3L)
+        ).getBeanOfType("test", Long.class);
 
         assertEquals(3L, bean.longValue());
     }
@@ -80,34 +63,32 @@ public class MultisourceBeanLocatorTest {
 
     @Test
     public void getBeanOfType_nullBothSources() {
-        when(source1.getBeanOfType(anyString(), eq(Long.class))).thenReturn(null);
-        when(source2.getBeanOfType(anyString(), eq(Long.class))).thenReturn(null);
-
-        Long bean = multisourceBeanLocator.getBeanOfType("test", Long.class);
+        Long bean = new MultisourceBeanLocator(
+                new BeanLocator(Long.class, null),
+                new BeanLocator(Long.class, null)
+        ).getBeanOfType("test", Long.class);
 
         assertNull(bean);
     }
 
     @Test
     public void getBeanOfType_notNullBothSources() {
-        when(source1.getBeanOfType(anyString(), eq(Long.class))).thenReturn(2L);
-        when(source2.getBeanOfType(anyString(), eq(Long.class))).thenReturn(3L);
 
-        Long bean = multisourceBeanLocator.getBeanOfType("test", Long.class);
+        Long bean = new MultisourceBeanLocator(
+                new BeanLocator(Long.class, 2L),
+                new BeanLocator(Long.class, 3L)
+        ).getBeanOfType("test", Long.class);
 
-        verifyNoMoreInteractions(source2);
         assertEquals(2L, bean.longValue());
     }
-
 
     @Test
     public void getBeansOfType_emptySource() {
 
-
-        when(source1.getBeansOfType(eq(Long.class))).thenReturn(emptyList());
-        when(source2.getBeansOfType(eq(Long.class))).thenReturn((Collection) asList(2L, 3L));
-
-        Collection<? extends Long> beans = multisourceBeanLocator.getBeansOfType(Long.class);
+        Collection<? extends Long> beans = new MultisourceBeanLocator(
+                new BeansLocator(Long.class, emptyList()),
+                new BeansLocator(Long.class, asList(2L, 3L))
+        ).getBeansOfType(Long.class);
 
         assertArrayEquals(asList(2L, 3L).toArray(), beans.toArray());
     }
@@ -115,11 +96,10 @@ public class MultisourceBeanLocatorTest {
     @Test
     public void getBeansOfType_nullSource() {
 
-
-        when(source1.getBeansOfType(eq(Long.class))).thenReturn(null);
-        when(source2.getBeansOfType(eq(Long.class))).thenReturn((Collection) asList(2L, 3L));
-
-        Collection<? extends Long> beans = multisourceBeanLocator.getBeansOfType(Long.class);
+        Collection<? extends Long> beans = new MultisourceBeanLocator(
+                new BeansLocator(Long.class, null),
+                new BeansLocator(Long.class, asList(2L, 3L))
+        ).getBeansOfType(Long.class);
 
         assertArrayEquals(asList(2L, 3L).toArray(), beans.toArray());
     }
@@ -127,72 +107,201 @@ public class MultisourceBeanLocatorTest {
 
     @Test
     public void getBeansOfType_mergeSource() {
-        when(source1.getBeansOfType(eq(Long.class))).thenReturn((Collection) asList(1L, 3L));
-        when(source2.getBeansOfType(eq(Long.class))).thenReturn((Collection) asList(2L, 3L));
-
-        Collection<? extends Long> beans = multisourceBeanLocator.getBeansOfType(Long.class);
+        Collection<? extends Long> beans = new MultisourceBeanLocator(
+                new BeansLocator(Long.class, asList(1L, 3L)),
+                new BeansLocator(Long.class, asList(2L, 3L))
+        ).getBeansOfType(Long.class);
 
         assertArrayEquals(asList(1L, 3L, 2L, 3L).toArray(), beans.toArray());
     }
 
     @Test
     public void loadBeansOfType() {
+        BeansLoader l1 = new BeansLoader();
+        BeansLoader l2 = new BeansLoader();
 
-        ConfiguredBeanLocator.BeanLoaderListener listener = mock(ConfiguredBeanLocator.BeanLoaderListener.class);
-        multisourceBeanLocator.loadBeansOfType(Long.class, listener);
+        ConfiguredBeanLocator.BeanLoaderListener listener = new ConfiguredBeanLocator.BeanLoaderListener() {
+            @Override
+            public boolean loadBean(String s, Class aClass) {
+                return false;
+            }
 
-        verify(source1).loadBeansOfType(eq(Long.class), eq(listener));
-        verify(source2).loadBeansOfType(eq(Long.class), eq(listener));
+            @Override
+            public boolean beanLoaded(String s, Object o) {
+                return false;
+            }
+        };
+
+        new MultisourceBeanLocator(l1, l2).loadBeansOfType(Long.class, listener);
+        assertSame(listener, l1.loadListener);
+        assertSame(listener, l2.loadListener);
     }
 
     @Test
     public void hasConfiguredPropertyValue_firstLocator() {
-        when(source1.hasConfiguredPropertyValue(eq("test"), eq("testProp"), eq("val"))).thenReturn(true);
-        when(source2.hasConfiguredPropertyValue(eq("test"), eq("testProp"), eq("val"))).thenReturn(false);
-
-        verifyNoMoreInteractions(source2);
-        assertTrue(multisourceBeanLocator.hasConfiguredPropertyValue("test", "testProp", "val"));
+        assertTrue(new MultisourceBeanLocator(
+                new HasPropertyValue("testProp"),
+                new HasPropertyValue("other")
+        ).hasConfiguredPropertyValue("test", "testProp", "val"));
     }
 
     @Test
     public void hasConfiguredPropertyValue_secondLocator() {
-        when(source1.hasConfiguredPropertyValue(eq("test"), eq("testProp"), eq("val"))).thenReturn(false);
-        when(source2.hasConfiguredPropertyValue(eq("test"), eq("testProp"), eq("val"))).thenReturn(true);
-
-        assertTrue(multisourceBeanLocator.hasConfiguredPropertyValue("test", "testProp", "val"));
+        assertTrue(new MultisourceBeanLocator(
+                new HasPropertyValue("other"),
+                new HasPropertyValue("testProp")
+        ).hasConfiguredPropertyValue("test", "testProp", "val"));
     }
 
     @Test
     public void hasConfiguredPropertyValue_noLocator() {
-        when(source1.hasConfiguredPropertyValue(eq("test"), eq("testProp"), eq("val"))).thenReturn(false);
-        when(source2.hasConfiguredPropertyValue(eq("test"), eq("testProp"), eq("val"))).thenReturn(false);
-
-        assertFalse(multisourceBeanLocator.hasConfiguredPropertyValue("test", "testProp", "val"));
+        assertFalse(new MultisourceBeanLocator(
+                new HasPropertyValue("other"),
+                new HasPropertyValue("other")
+        ).hasConfiguredPropertyValue("test", "testProp", "val"));
     }
 
 
     @Test
     public void hasBeanOfName_firstLocator() {
-        when(source1.hasBeanOfName(eq("test"))).thenReturn(true);
-        when(source2.hasBeanOfName(eq("test"))).thenReturn(false);
-
-        verifyNoMoreInteractions(source2);
-        assertTrue(multisourceBeanLocator.hasBeanOfName("test"));
+        assertTrue(new MultisourceBeanLocator(
+                new HasBeanOfName("test"),
+                new HasBeanOfName("other")
+        ).hasBeanOfName("test"));
     }
 
     @Test
     public void hasBeanOfName_secondLocator() {
-        when(source1.hasBeanOfName(eq("test"))).thenReturn(false);
-        when(source2.hasBeanOfName(eq("test"))).thenReturn(true);
-
-        assertTrue(multisourceBeanLocator.hasBeanOfName("test"));
+        assertTrue(new MultisourceBeanLocator(
+                new HasBeanOfName("other"),
+                new HasBeanOfName("test")
+        ).hasBeanOfName("test"));
     }
 
     @Test
     public void hasBeanOfName_noLocator() {
-        when(source1.hasBeanOfName(eq("test"))).thenReturn(false);
-        when(source2.hasBeanOfName(eq("test"))).thenReturn(false);
+        assertFalse(new MultisourceBeanLocator(
+                new HasBeanOfName("other"),
+                new HasBeanOfName("other")
+        ).hasBeanOfName("test"));
+    }
 
-        assertFalse(multisourceBeanLocator.hasBeanOfName("test"));
+    static class BeanNamesLocator extends TestBeanLocator {
+
+        private final Class<?> type;
+        private final List<String> beanNames;
+
+        public BeanNamesLocator(Class<?> type, List<String> beanNames) {
+            this.type = type;
+            this.beanNames = beanNames;
+        }
+
+        @Override
+        public List<String> getBeanNamesOfType(Class<?> aClass) {
+            return type.equals(aClass) ? beanNames : List.of();
+        }
+    }
+
+    static class BeanLocator extends TestBeanLocator {
+
+        private final Class<?> type;
+        private final Object bean;
+
+        public BeanLocator(Class<?> type, Object bean) {
+            this.type = type;
+            this.bean = bean;
+        }
+
+        @Override
+        public <T> T getBeanOfType(String s, Class<T> aClass) {
+            return type.equals(aClass) ? (T) bean : null;
+        }
+    }
+
+    static class BeansLocator extends TestBeanLocator {
+
+        private final Class<?> type;
+        private final List<Object> beans;
+
+        public BeansLocator(Class<?> type, List<Object> beans) {
+            this.type = type;
+            this.beans = beans;
+        }
+
+        @Override
+        public <T> Collection<? extends T> getBeansOfType(Class<T> aClass) {
+            return type.equals(aClass) ? (Collection<? extends T>) beans : List.of();
+        }
+    }
+
+    static class BeansLoader extends TestBeanLocator {
+        BeanLoaderListener loadListener;
+
+        @Override
+        public <T> boolean loadBeansOfType(Class<T> aClass, BeanLoaderListener<T> beanLoaderListener) {
+            this.loadListener = beanLoaderListener;
+            return false;
+        }
+    }
+
+    static class HasPropertyValue extends TestBeanLocator {
+
+        private final String name;
+
+        public HasPropertyValue(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean hasConfiguredPropertyValue(String s, String s1, String s2) {
+            return s1.equals(name);
+        }
+    }
+
+    static class HasBeanOfName extends TestBeanLocator {
+
+        private final String name;
+
+        public HasBeanOfName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean hasBeanOfName(String s) {
+            return s.equals(name);
+        }
+    }
+
+    static class TestBeanLocator implements ConfiguredBeanLocator {
+
+        @Override
+        public List<String> getBeanNamesOfType(Class<?> aClass) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public <T> T getBeanOfType(String s, Class<T> aClass) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public <T> Collection<? extends T> getBeansOfType(Class<T> aClass) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public <T> boolean loadBeansOfType(Class<T> aClass, BeanLoaderListener<T> beanLoaderListener) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean hasConfiguredPropertyValue(String s, String s1, String s2) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean hasBeanOfName(String s) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
